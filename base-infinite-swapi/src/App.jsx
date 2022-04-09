16aa8fa7aaecb80ed26360a99c714aca7a58310a
sksurendrakmr <sksurendrakmr@gmail.com>
2022-04-09 23:32:29 +0530

    InfiniteQuery Notes and InfinitePeople components

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,14 +1,68 @@
 import "./App.css";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
 
+/**
+ * useInfiniteQuery vs useQuery
+ * 1. In useInfiniteQuery, shape of data different than usequery.
+ * 2. Data Object with two properties :-
+ *      --> pages (array of objects for each page of data)
+ *      --> pageParams
+ * 3. Every query has its own element in the pages array.
+ * 4. pageParams tracks the keys of queries that have been retieved.
+ *    --> rarely used.
+ *
+ * Syntax
+ * -> pageParam is a parameter passed to the queryFn.
+ * --> useInfiniteQuery("sw-people", ({pageParam = defaultUrl}) => fetchUrl(pageParam))
+ *
+ * ==========
+ *
+ * useInfiniteQuery options
+ *
+ * Funtion that determine how to get the next page.
+ * Either from the data from lastPage or the data from the allPages.
+ *
+ * --> getNextPageParam : (lastPage, allPages)
+ *      --> Updates pageParam
+ *      --> Might use all of the pages of data (allPages)
+ *
+ *
+ * There are also some properties of the return object that are different from useQuery.
+ * 
+ *
+   1. fetchNextPage -> a function we need to call whenever the user needs more data. 
+                     (Either to click a button or when they hit the point on the screen 
+                      where they are about to run out of data)
+
+   2. hasNextPage -> Based in return value of getNextPageParam
+                     if undefined, no more data.
+
+   3. isFetchingNextPage -> For displaying a loader spinner
+                            useInfinteQuery can distinguish between whether it's fetching
+                            the next page or whether it's fetching in general.
+        
+
+      ==================================
+      The flow
+      ==============
+      When componentMount, data property of useInfinteQuery is undefined as we haven't made query yet.
+ * 
+ */
+
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        {/* <InfiniteSpecies /> */}
+        <ReactQueryDevtools position="bottom-right" initialIsOpen={false} />
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,13 +1,66 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "react-query";
 import { Person } from "./Person";
 
+/**
+ * fetchNextPage -> determine which function to run when we want more data.
+ * hasNextPage -> boolean, determing whether we have more data to collect or not.
+ */
+
 const initialUrl = "https://swapi.dev/api/people/";
-const fetchUrl = async (url) => {
-  const response = await fetch(url);
+const fetchUrl = async ({ pageParam = initialUrl }) => {
+  const response = await fetch(pageParam);
   return response.json();
 };
 
+const formatResults = (results) => {
+  return results.map((result) => ({
+    name: result.name,
+    hairColor: result.hair_color,
+    eyeColor: result.eye_color,
+  }));
+};
 export function InfinitePeople() {
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isError,
+    error,
+    isFetchingNextPage,
+  } = useInfiniteQuery("sw-people", fetchUrl, {
+    getNextPageParam: (lastPage, allPages) => lastPage.next || undefined,
+    select: (data) => {
+      return data.pages.map((person) => {
+        return {
+          ...person,
+          results: formatResults(person.results),
+        };
+      });
+    },
+  });
+
+  if (isLoading) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  if (isError) {
+    return <div className="">{error.toString()}</div>;
+  }
+
+  console.log(data);
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  return (
+    <>
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.map((person) => {
+          return person.results.map((result) => {
+            return <Person key={result.name} {...result} />;
+          });
+        })}
+      </InfiniteScroll>
+      {isFetchingNextPage && <div>Loading...</div>}
+    </>
+  );
 }
