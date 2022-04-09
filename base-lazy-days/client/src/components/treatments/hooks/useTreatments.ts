aa4ab414c93e2dc10e3592cc7d1907f90409e711
sksurendrakmr <sksurendrakmr@gmail.com>
2022-04-10 01:00:53 +0530

    custom query hook - useTreatments and fallback data

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,15 +1,19 @@
+import { useQuery } from 'react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
 
 export function useTreatments(): Treatment[] {
-  // TODO: get data from server via useQuery
-  return [];
+  //we will going to handle error and isFetching, isLoading etc centrally at one place
+  //So, here we will going to have fallback value for data
+  const fallback = []
+  const {data=fallback} = useQuery(queryKeys.treatments,getTreatments)
+  return data;
 }
