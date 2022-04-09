03509e975d4bec86193111b61589c16bf94237c1
sksurendrakmr <sksurendrakmr@gmail.com>
2022-04-10 01:37:13 +0530

    onError Default for query client

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -2,7 +2,6 @@ import { useQuery } from 'react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -11,15 +10,9 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
-  const toast = useCustomToast();
   //we will going to handle error and isFetching, isLoading etc centrally at one place
   //So, here we will going to have fallback value for data
   const fallback = []
-  const {data=fallback} = useQuery(queryKeys.treatments,getTreatments,{
-    onError:(error)=>{
-      const title = error instanceof Error ? error.message : "error connecting to the server";
-      toast({title,status:'error'})
-    }
-  })
+  const {data=fallback} = useQuery(queryKeys.treatments,getTreatments)
   return data;
 }
