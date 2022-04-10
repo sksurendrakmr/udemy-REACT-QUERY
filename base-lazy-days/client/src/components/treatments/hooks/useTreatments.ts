2ea01b9cde894f7441d4cc1f501771b33562743a
sksurendrakmr <sksurendrakmr@gmail.com>
2022-04-10 13:32:30 +0530

    Prefetch and useStaff excercise

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
@@ -15,4 +15,15 @@ export function useTreatments(): Treatment[] {
   const fallback = []
   const {data=fallback} = useQuery(queryKeys.treatments,getTreatments)
   return data;
+}
+
+//Will call usePrefetchTreatments from Home component. That way
+//the data will be preloaded into the cache and as long as the user goes
+//to the treatments page before the cache time is up, then we will be able 
+//to display the cached data and user's don't have to wait for us actually
+//to make a server call.
+
+export function usePrefetchTreatments():void {
+  const queryClient = useQueryClient();
+  queryClient.prefetchQuery(queryKeys.treatments,getTreatments)
 }
