a11f0face8754596ad6527823e1d7ba8043832c8
sksurendrakmr <sksurendrakmr@gmail.com>
2022-04-10 18:45:48 +0530

    Test rendered Staff Data

--- base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -3,7 +3,20 @@ import { renderWithQueryClient } from "test-utils";
 
 import { Treatments } from "../Treatments";
 
-test("renders response from query", () => {
+test("renders response from query", async () => {
   // write test here
   renderWithQueryClient(<Treatments />);
+  //will look at the response that we are getting from
+  //the query and not just renders without errors.
+
+  //marked with async because it will actually going to render
+  //the response from the query asynchronously even though its not
+  //making a network request as mock service worker request is still asynchronously.
+
+  //test whether it render query response or not
+  const treatmentTitles = await screen.findAllByRole("heading", {
+    name: /massage|facial|scrub/i,
+  });
+
+  expect(treatmentTitles).toHaveLength(3);
 });
